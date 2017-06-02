aa2ba0a511586508873f06151e8395b6c8a4c74d
kmmx <kmmx@hsoft.local>
2017-06-02 09:46:47 -0500

    Sesión 3

--- sesion_3/strings.3.py
+++ sesion_3/strings.3.py
@@ -2,30 +2,16 @@
 
 s = "a = 123 + 246 * (124 + 16 * 2) + (24 * 38 - 12) / (54 - 29 + 1)"
 
-# We need a list ["124 + 16 * 2", "24 * 38 - 12", "54 - 29 + 1"]
+inside = False # Inside a parenthesis
 
-# We need a loop
+sub = ""
 
-# Syntax: 
-# while <condition>:
-#   block
-
-# Repeat the loop forever
-i = 0 # Start the search from 0
-while True:
-    # To handle a possible exception we need to embed the code that raise an exception
-    # try - except
-    try:
-        si = s.index("(", i)
-        ei = s.index(")", i)
-    except: # There is an exception
-        print "They are not more (·)"
-        break # Break the infinite loop (break the while)
-    else: # There is not an exception
-        sub = s[si+1:ei]
-        print sub # TO-DO: Add the sub into a list
-        i = ei + 1
-
-    # Other code
-
-# Continue here
+for c in s:
+    if c == "(":
+        inside = True
+    elif c == ")":
+        print sub
+        sub = "" # Reset
+        inside = False
+    elif inside: # Check if inside == True
+        sub += c # sub = sub + c <concat>
