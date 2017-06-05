69bec84572c6b4e96c910ebc825a5f51a6f845ad
kmmx <kmmx@hsoft.local>
2017-06-05 09:58:56 -0500

    Sesión 4

---
+++ sesion_4/ej1.py
@@ -0,0 +1,50 @@
+import random
+
+names = ["Ana", "Beto", "Carlos", "Daniel", "Edna", "Fatima", "Gerardo"]
+lasts = ["Alvarez", "Benitez", "Castillo", "Diaz", "Escamilla", "Fernandez"]
+
+fullnames = []
+
+# Make a list of fake data
+for i in range(100):
+    n = random.choice(names) # Choose randomly an element
+    l = random.choice(lasts)
+
+    s = "%s %s" % (n, l)
+
+    fullnames.append(s)
+
+print fullnames
+
+people = []
+
+genders = ["Male", "Female", "Not defined"]
+
+streets = ["Rojo Gomez", "Reforma", "Bicentenario", "Juarez"]
+neighborhoods = ["Condesa", "Roma Norte", "Narvarte"]
+
+for name in fullnames:
+    d = {
+        "name": name, # In the "name" key, storage the <name> value
+        "age": random.randint(0, 80), # Generate a random value between 0 and 80
+        "gender": random.choice(genders),
+        "address": {
+            "street": random.choice(streets),
+            "number": random.randint(1, 400),
+            "neighborhood": random.choice(neighborhoods),
+            "city": "Mexico City",
+            "zipcode": random.randint(10000, 99999)
+        }
+    }
+
+    people.append(d)
+
+print people
+
+# Parse <people> data into a json's string
+import json
+
+# Code to json
+people_json = json.dumps(people)
+
+print people_json
